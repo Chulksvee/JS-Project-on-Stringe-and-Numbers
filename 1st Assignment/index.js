747847765a5dbcc85ed5269c4b51c5cea5485504
Chulksvee_Tech <148911623+Chulksvee@users.noreply.github.com>
2024-03-27 23:33:48 -0400

    Add files via upload
    This is a project on Stringe and Numbers on JS

---
+++ 1st Assignment/index.js
@@ -0,0 +1,15 @@
+// code checks the datatype that is inputed in the prompt
+
+
+let number = prompt("Input a number or a string here");
+if (number = parseInt(number)){
+
+document.write ('you inputed a number')
+
+}
+
+else{
+
+document.write('you inputed a string')
+
+}
